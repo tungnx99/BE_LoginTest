8da09872d20c7631114cf0e2d535883f3fbd28fe
tungnx <tung.nx4388@sinhvien.hoasen.edu.vn>
2021-03-16 10:36:22 +0700

    dev-final login be local test

---
+++ BE/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using BE.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // GET: api/<AccountController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AccountController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        public JsonResult Post([FromBody] Account data)
+        {
+            if (data.UserName != "admin")
+            {
+                return new JsonResult(false);
+            }
+            if (data.Password != "123456")
+            {
+                return new JsonResult(false);
+            }
+            return new JsonResult(true);
+        }
+
+        // PUT api/<AccountController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
